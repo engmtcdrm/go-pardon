c9a258a094ba548d4f536464ec943a644d776830
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-07-12 14:26:14 -0400

    reoganized numerically by byte

--- constants.go
+++ constants.go
@@ -1,14 +1,13 @@
 package gocliselect
 
 const (
-
 	// Key codes for navigation and actions
+	KeyCtrlC          = byte(3)  // Ctrl+C generates ASCII 3 in raw mode
+	KeyCarriageReturn = byte(10) // Additional for cross-platform compatibility
+	KeyEnter          = byte(13)
+	KeyEscape         = byte(27)
 	KeyUp             = byte(65)
 	KeyDown           = byte(66)
-	KeyEscape         = byte(27)
-	KeyEnter          = byte(13)
-	KeyCarriageReturn = byte(10) // Additional for cross-platform compatibility
-	KeyCtrlC          = byte(3)  // Ctrl+C generates ASCII 3 in raw mode
 )
 
 // NavigationKeys defines a map of specific byte keycodes related to
