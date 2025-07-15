4fb501ac529a6d07dfdb32db401e463bd4268d85
John <deedsoftheforsaken@gmail.com>
2025-07-15 15:41:52 -0400

    Added more key codes

--- keycodes.go
+++ keycodes.go
@@ -2,14 +2,18 @@ package gocliselect
 
 const (
 	// Key codes for navigation and actions
-	KeyCtrlC          = byte(3)  // Ctrl+C generates ASCII 3 in raw mode
+	KeyCtrlC          = byte(3)
+	KeyDelete         = byte(8)
 	KeyCarriageReturn = byte(10) // Additional for cross-platform compatibility
 	KeyEnter          = byte(13)
 	KeyEscape         = byte(27)
 	KeyUp             = byte(65)
 	KeyDown           = byte(66)
+	KeyRight          = byte(67)
+	KeyLeft           = byte(68)
 	KeyNoUpper        = byte(78)
 	KeyYesUpper       = byte(89)
 	KeyNo             = byte(110)
 	KeyYes            = byte(121)
+	KeyBackspace      = byte(127)
 )
