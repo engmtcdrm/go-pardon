f16a2242adbc3c973fd69031df27cb65960e7821
John <deedsoftheforsaken@gmail.com>
2025-07-18 10:44:52 -0400

    Added defaultFn along with function now requiring passing the type as the main argument

--- evalval.go
+++ evalval.go
@@ -2,8 +2,9 @@ package pardon
 
 // evalVal is a struct that holds a value and an optional function to retrieve it dynamically.
 type evalVal[T any] struct {
-	val T
-	fn  func() T
+	val       T
+	fn        func(T) T
+	defaultFn func(T) T
 }
 
 // Get retrieves the value.
@@ -11,7 +12,11 @@ type evalVal[T any] struct {
 // returns the stored value.
 func (d *evalVal[T]) Get() T {
 	if d.fn != nil {
-		return d.fn()
+		return d.fn(d.val)
+	}
+
+	if d.defaultFn != nil {
+		return d.defaultFn(d.val)
 	}
 
 	return d.val
