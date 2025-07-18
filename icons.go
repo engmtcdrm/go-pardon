0b737596ac647599685ca336a41c2247c25c8074
John <deedsoftheforsaken@gmail.com>
2025-07-17 20:56:20 -0400

    Added Alert icon

--- icons.go
+++ icons.go
@@ -1,12 +1,15 @@
 package pardon
 
 type icons struct {
+	Alert        string
 	QuestionMark string
 	Password     string
 }
 
-// Default icons available in the package
+// Default icons available in the package. Overwrite these with
+// custom icons using the Icon or IconFunc methods in the respective structs.
 var Icons = icons{
+	Alert:        "[!] ",
 	QuestionMark: "[?] ",
 	Password:     "🔒 ",
 }
