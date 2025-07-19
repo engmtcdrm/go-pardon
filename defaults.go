526a81479c219df5f261ce2e0853cc8faf90b6ef
John <deedsoftheforsaken@gmail.com>
2025-07-18 21:00:38 -0400

    Merged icons into defaults file

--- defaults.go
+++ defaults.go
@@ -1,5 +1,19 @@
 package pardon
 
+type icons struct {
+	Alert        string
+	QuestionMark string
+	Password     string
+}
+
+// Default icons available in the package. Overwrite these with
+// custom icons using the Icon or IconFunc methods in the respective structs.
+var Icons = icons{
+	Alert:        "[!] ",
+	QuestionMark: "[?] ",
+	Password:     "🔒 ",
+}
+
 type funcs struct {
 	answerFn func(string) string
 	cursorFn func(string) string
