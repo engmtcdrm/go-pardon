a36c157dcf2d47b00393065e884406c81fb5ee25
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-05-22 21:07:28 -0400

    chore: Additional functionality to prompt

--- newprompt.go
+++ newprompt.go
@@ -3,7 +3,6 @@ package pardon
 import (
 	"errors"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/engmtcdrm/go-ansi"
@@ -32,6 +31,8 @@ type InputPrompt struct {
 // NewStringPrompt creates an InputPrompt for plaintext string input.
 func NewStringPrompt(value *string) *InputPrompt {
 	return &InputPrompt{
+		icon:       eval[string]{val: Icons.QuestionMark, defaultFn: defaultFuncs.iconFn},
+		title:      eval[string]{val: "", defaultFn: defaultFuncs.titleFn},
 		answerFn:   func(s string) string { return s },
 		validateFn: func(s string) error { return nil },
 		hide:       false,
@@ -43,33 +44,9 @@ func NewStringPrompt(value *string) *InputPrompt {
 // NewPasswordPrompt creates an InputPrompt for secure password input with masking.
 func NewPasswordPrompt(value *string) *InputPrompt {
 	inputPrompt := NewStringPrompt(value)
+	inputPrompt.icon = eval[string]{val: Icons.Password, defaultFn: defaultFuncs.iconFn}
 	inputPrompt.Hide(true)
 	return inputPrompt
-}
-
-func (p *InputPrompt) Hide(hide bool) *InputPrompt {
-	p.hide = hide
-	p.input.Hide = hide
-	return p
-}
-
-// Title sets the question text.
-func (p *InputPrompt) Title(title string) *InputPrompt {
-	p.title.val = title
-	return p
-}
-
-// TitleFunc sets a dynamic title function.
-func (p *InputPrompt) TitleFunc(fn func(string) string) *InputPrompt {
-	p.title.fn = fn
-	return p
-}
-
-func (p *InputPrompt) Validate(fn func(string) error) *InputPrompt {
-	if fn != nil {
-		p.validateFn = fn
-	}
-	return p
 }
 
 func (p *InputPrompt) AnswerFunc(fn func(string) string) *InputPrompt {
@@ -79,9 +56,9 @@ func (p *InputPrompt) AnswerFunc(fn func(string) string) *InputPrompt {
 	return p
 }
 
-// Value sets a default input value.
-func (p *InputPrompt) Value(value *string) *InputPrompt {
-	p.value = value
+func (p *InputPrompt) Hide(hide bool) *InputPrompt {
+	p.hide = hide
+	p.input.Hide = hide
 	return p
 }
 
@@ -98,6 +75,33 @@ func (p *InputPrompt) IconFunc(fn func(string) string) *InputPrompt {
 	return p
 }
 
+// Title sets the prompt text.
+func (p *InputPrompt) Title(title string) *InputPrompt {
+	p.title.val = title
+	return p
+}
+
+// TitleFunc sets a dynamic title function.
+func (p *InputPrompt) TitleFunc(fn func(string) string) *InputPrompt {
+	p.title.fn = fn
+	return p
+}
+
+// Value sets a default input value.
+func (p *InputPrompt) Value(value *string) *InputPrompt {
+	p.value = value
+	return p
+}
+
+// ValidateFunc sets a validation function for the prompt input.
+func (p *InputPrompt) ValidateFunc(fn func(string) error) *InputPrompt {
+	if fn != nil {
+		p.validateFn = fn
+	}
+	return p
+}
+
+// Ask displays the prompt and waits for input.
 func (p *InputPrompt) Ask() error {
 	if p.title.val == "" && p.title.fn == nil {
 		return ErrNoTitle
@@ -109,14 +113,15 @@ func (p *InputPrompt) Ask() error {
 
 	p.prompt = fmt.Sprintf("%s%s ", p.icon.Get(), p.title.Get())
 
-	err := p.ask()
-	if err != nil {
+	if err := p.ask(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+// ask handles the core logic of displaying the prompt, reading user input,
+// validating it, and applying the answer transformation.
 func (p *InputPrompt) ask() error {
 	fmt.Print(p.prompt)
 
@@ -153,39 +158,58 @@ func (p *InputPrompt) printErrorMessage(err error) {
 		builder.WriteByte('\n')
 	}
 
-	builder.WriteString(buildValidationErrorMessage(err))
-	builder.WriteString(ansi.CursorUp(1) + ansi.ClearLineReset)
+	// Write error message, move/clear the line above, then reprint the prompt.
+	errMsg := validationErrorMessage(err)
+
+	errMsgLines, err := p.getPromptLines(errMsg)
+	if err != nil {
+		panic(err)
+	}
+
+	builder.WriteString(errMsg)
+	for i := 0; i < errMsgLines-1; i++ {
+		builder.WriteString(ansi.CursorUp(1))
+	}
+	builder.WriteString(resetLineAbove())
 	builder.WriteString(p.prompt)
 	fmt.Print(builder.String())
 }
 
 func (p *InputPrompt) printFinalPromptLine() {
 	builder := strings.Builder{}
-	// If the input is not hidden, let's redraw the prompt and call the answer
-	// function to look pretty.
+	// If the input is not hidden, cursor will be on the next line due to user
+	// pressing enter. We need to move the cursor up, clear, then print the
+	// prompt with the answer function applied.
 	if !p.hide {
-		builder.WriteString(ansi.CursorUp(1) + ansi.ClearLineReset)
+		builder.WriteString(resetLineAbove())
 		builder.WriteString(p.prompt + p.answerFn(*p.value))
 	}
 
+	// Regardless of being hidden or not we need to move to the next line and
+	// clear it in case there are any validation error messages that are still
+	// visible.
 	builder.WriteString("\n" + ansi.ClearLineReset)
 	fmt.Print(builder.String())
 }
 
-func buildValidationErrorMessage(err error) string {
-	return fmt.Sprintf("%s%s* %s%s", ansi.ClearLineReset, ansi.Red, err.Error(), ansi.Reset)
+func validationErrorMessage(err error) string {
+	return fmt.Sprintf("%s%s* %v%s", ansi.ClearLineReset, ansi.RedBg, err, ansi.Reset)
 }
 
-func getPromptLines(prompt string) (int, error) {
+func resetLineAbove() string {
+	return ansi.CursorUp(1) + ansi.ClearLineReset
+}
+
+func (p *InputPrompt) getPromptLines(prompt string) (int, error) {
 	promptLines := 1
 
-	fd := int(os.Stdin.Fd())
+	fd := int(p.input.reader.Fd())
 	width, _, err := term.GetSize(fd)
 	if err != nil {
 		return 0, err
 	}
 
-	promptCharCnt := len(ansi.StripCodes(prompt))
+	promptCharCnt := len(ansi.Strip(prompt))
 
 	// If prompt is wider than terminal calculate number of lines it will take
 	if promptCharCnt > width {
