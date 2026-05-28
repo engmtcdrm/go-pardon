7cda0fcbf10eada04ce8834d2b7f4da6d60ad3cf
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-05-27 21:26:51 -0400

    chore: added runekeys package

---
+++ internal/runekeys/runekeys.go
@@ -0,0 +1,27 @@
+package runekeys
+
+const (
+	CtrlC     = rune(3)
+	Backspace = rune(8)
+	Escape    = rune(27)
+	Delete    = rune(127)
+
+	Up    = rune('A')
+	Down  = rune('B')
+	Right = rune('C')
+	Left  = rune('D')
+
+	Enter       = rune('\r')
+	LeftBracket = rune('[')
+	LowerN      = rune('n')
+	LowerY      = rune('y')
+	NewLine     = rune('\n') // Additional for cross-platform compatibility
+
+	UpperA = rune('A')
+	UpperB = rune('B')
+	UpperC = rune('C')
+	UpperD = rune('D')
+	UpperN = rune('N')
+	UpperO = rune('O')
+	UpperY = rune('Y')
+)
