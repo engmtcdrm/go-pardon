48cdcffb09f6651abbdf096d93842165e4dcf1a2
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-06-04 21:28:35 -0400

    refactor: changed text of ErrNoValue

--- errors.go
+++ errors.go
@@ -8,6 +8,6 @@ var (
 	ErrNoPrompts       = errors.New("form requires at least one prompt")
 	ErrNoSelectOptions = errors.New("select prompt requires at least one option")
 	ErrNoTitle         = errors.New("prompt requires a title")
-	ErrNoValue         = errors.New("value must be set")
+	ErrNoValue         = errors.New("prompt requires a value")
 	ErrUserAborted     = errors.New("user aborted")
 )
