eec7a05ba69921dc5a6f5562732dcf3e64a89af3
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-06-07 13:24:19 -0400

    chore: Updated comments on eval struct

--- eval.go
+++ eval.go
@@ -2,12 +2,18 @@ package pardon
 
 // eval holds a value that can be statically set or dynamically computed.
 type eval[T any] struct {
-	val       T         // Static value
-	fn        func(T) T // Optional transformation function
-	defaultFn func(T) T // Fallback transformation function
+	// Static value.
+	val T
+
+	// Optional transformation function.
+	fn func(T) T
+
+	// Fallback transformation function.
+	defaultFn func(T) T
 }
 
-// Get returns the evaluated value, applying fn or defaultFn if set.
+// Get returns the evaluated value, applying the transformation function if it
+// exists. If no transformation function is set, it returns the static value.
 func (d *eval[T]) Get() T {
 	if d.fn != nil {
 		return d.fn(d.val)
