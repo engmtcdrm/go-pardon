15329da9395f47f43a5bd5379ad678358f134eca
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-06-17 21:24:11 -0400

    chore: Added ClusterSetFromString function

--- grapheme/utils.go
+++ grapheme/utils.go
@@ -1,6 +1,71 @@
 package grapheme
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/clipperhouse/uax29/v2/graphemes"
+)
+
+func ClusterSetFromString(s string) ClusterSet {
+	var pendingSet ClusterSet
+	gr := graphemes.FromString(s)
+
+	for gr.Next() {
+		cluster := New([]rune(gr.Value())...)
+		pendingSet = append(pendingSet, cluster)
+	}
+
+	var cs ClusterSet
+
+	for len(pendingSet) > 0 {
+		c := pendingSet[0]
+
+		if !Equal(c, Escape) {
+			cs = append(cs, c)
+			pendingSet = pendingSet[1:]
+			continue
+		}
+
+		pendingEscSeqSet := ClusterSet{}
+		pendingEscSeqSet = append(pendingEscSeqSet, c)
+		if len(pendingSet) == 1 {
+			// We have an escape character but no more input, so we should treat
+			// it as a normal cluster.
+			cs = append(cs, c)
+			pendingSet = pendingSet[1:]
+			continue
+		}
+
+		pendingEscSeqSet = append(pendingEscSeqSet, pendingSet[1])
+		pendingEscSeqCluster := New(pendingEscSeqSet.Runes()...)
+		switch {
+		case !IsFeEscapeSequence(pendingEscSeqCluster):
+			// We have an escape character and one more input, but it's not a
+			// full escape sequence, so we should treat the escape character as
+			// a normal cluster and continue processing the next cluster.
+			cs = append(cs, c)
+			pendingSet = pendingSet[1:]
+			continue
+		}
+
+		nextCluster := New(pendingSet[2:].Runes()...)
+		seqEndIdx := IndexOfSequenceEnd(nextCluster)
+		if seqEndIdx == -1 {
+			// We have the start of an escape sequence but we don't have the
+			// full sequence yet, so we should treat the escape character as a
+			// normal cluster and continue processing the next cluster.
+			cs = append(cs, c)
+			pendingSet = pendingSet[1:]
+			continue
+		}
+
+		pendingEscSeqCluster = append(pendingEscSeqCluster, pendingSet[2:3+seqEndIdx].Runes()...)
+		// cs = append(cs, pendingEscSeqCluster)
+		pendingSet = pendingSet[3+seqEndIdx:]
+	}
+
+	return cs
+}
 
 // Equal reports whether a and b are the same length and contain the same runes.
 // A nil argument is equivalent to an empty slice.
